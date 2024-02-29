e5476f0f177f5f349c6b808613e0013607c70386
jainil-weboccult <jainil.solanki@weboccult.com>
2024-02-29 12:50:52 +0530

    removed fail code

--- src/app/(modules)/app/friends/page.tsx
+++ src/app/(modules)/app/friends/page.tsx
@@ -1,16 +1,7 @@
-import { FriendAPI } from "@/app/services/axios/apis/friend.api";
 import AppWrapper from "@/app/wrappers/app.wrapper";
 import React from "react";
 
-function getAllfriends() {
-  const response = FriendAPI.getAllFriends();
-
-  return response;
-}
 export default async function Friends() {
-  const data = await getAllfriends();
-
-  console.log(data);
   return (
     <AppWrapper>
       <h1 style={{ margin: "auto", textAlign: "center" }}>Only Friends</h1>
