64d07aaacbaaf6ade2fcc0fc4936aeb4aa83bd00
jainil-weboccult <jainil.solanki@weboccult.com>
2024-04-02 17:21:44 +0530

    created electron app

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
     //         allowedOrigins: ["https://6n3kq20k-3000.inc1.devtunnels.ms", "localhost:3000"]
     //     }
     // }
+    output: "export",
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
