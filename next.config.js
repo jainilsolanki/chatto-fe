790864f71ef0d9d8ab72059d89187b4beb0ebabc
jainil-weboccult <jainil.solanki@weboccult.com>
2024-04-04 10:56:57 +0530

    completed pwa setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
@@ -7,6 +11,7 @@ const nextConfig = {
     //         allowedOrigins: ["https://6n3kq20k-3000.inc1.devtunnels.ms", "localhost:3000"]
     //     }
     // }
+
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig)
