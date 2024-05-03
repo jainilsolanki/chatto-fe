3c3e0e295e6a91d2dd4f49e0c33c782f6526d79c
jainil-weboccult <jainil.solanki@weboccult.com>
2024-05-03 13:34:38 +0530

    keeping service worker on halt

--- public/csw.js
+++ public/csw.js
@@ -1,22 +1,83 @@
+// self.addEventListener('install', (e) => {
+//     console.log("INSTALLED", e);
+//     // Skip Waiting
+//     self.skipWaiting()
+// })
+// self.addEventListener('activate', (e) => {
+//     console.log("Worker Activated", e);
+//     self.registration.showNotification("MAGIC", {
+//         body: "Buzz! Buzz!",
+//         icon: "https://static8.depositphotos.com/1012223/980/i/950/depositphotos_9803930-stock-photo-demo-cubes.jpg",
+//         vibrate: [200, 100, 200, 100, 200, 100, 200],
+//         tag: "vibration-sample",
+//     });
+//     setTimeout(() => {
+//         self.registration.showNotification("BACKGROUND BABY!", {
+//             body: "Buzz! Buzz!",
+//             icon: "https://static8.depositphotos.com/1012223/980/i/950/depositphotos_9803930-stock-photo-demo-cubes.jpg",
+//             vibrate: [200, 100, 200, 100, 200, 100, 200],
+//             tag: "vibration-sample",
+//         });
+//     }, 5000);
+// })
+
+
+// import {io} from 'socket.io-client';
+// console.log("OL", io)
+
 self.addEventListener('install', (e) => {
     console.log("INSTALLED", e);
     // Skip Waiting
     self.skipWaiting()
 })
-self.addEventListener('activate', (e) => {
+
+const urlB64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    } 
+    return outputArray
+  }
+ 
+self.addEventListener('activate', async(e) => {
     console.log("Worker Activated", e);
+    try {
+        const options = {userVisibleOnly: true, applicationServerKey: urlB64ToUint8Array(
+            'BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk'
+          )}
+
+        const subscription = await self.registration.pushManager.subscribe(options)
+        console.log(JSON.stringify(subscription))
+      } catch (err) {
+        console.log('Error', err)
+      }
     self.registration.showNotification("MAGIC", {
         body: "Buzz! Buzz!",
         icon: "https://static8.depositphotos.com/1012223/980/i/950/depositphotos_9803930-stock-photo-demo-cubes.jpg",
         vibrate: [200, 100, 200, 100, 200, 100, 200],
         tag: "vibration-sample",
     });
+
     setTimeout(() => {
         self.registration.showNotification("BACKGROUND BABY!", {
             body: "Buzz! Buzz!",
             icon: "https://static8.depositphotos.com/1012223/980/i/950/depositphotos_9803930-stock-photo-demo-cubes.jpg",
             vibrate: [200, 100, 200, 100, 200, 100, 200],
             tag: "vibration-sample",
-        });
+        }); 
     }, 5000);
 })
+
+self.addEventListener('beforeunload', (event) => {
+    event.preventDefault();
+    self.registration.showNotification("BYE BYE", {
+        body: "Buzz! Buzz!",
+        icon: "https://static8.depositphotos.com/1012223/980/i/950/depositphotos_9803930-stock-photo-demo-cubes.jpg",
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        tag: "vibration-sample",
+    });
+    return; // This will keep the service worker running
+  });
--- src/app/providers/next-auth-session-provider.tsx
+++ src/app/providers/next-auth-session-provider.tsx
@@ -1,23 +1,25 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function NextAuthSessionProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    navigator.serviceWorker.register("/csw.js").then(
+      (registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration
+        );
+      },
+      (err) => {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  }, []);
   return <SessionProvider>{children}</SessionProvider>;
 }
-
-navigator.serviceWorker.register("/csw.js").then(
-  (registration) => {
-    console.log(
-      "ServiceWorker registration successful with scope: ",
-      registration
-    );
-  },
-  (err) => {
-    console.log("ServiceWorker registration failed: ", err);
-  }
-);
